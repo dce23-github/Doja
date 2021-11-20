00f57abb96811ba75aa9c83433bb062b2b4e7408
dce23-git <dhirenchugh43@gmail.com>
2021-11-20 19:50:45 +0530

    added start script in package.json

--- judge/app.js
+++ judge/app.js
@@ -5,7 +5,7 @@ const methodOverride = require("method-override");
 const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
-const PORT = process.env.PORT || 8080;
+const PORT =  8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
